313b34c82bb194a9a39084d0a074f3643ff723b2
LaurentBarres <yoannpedase59@outlook.com>
2025-05-23 09:46:47 +0200

    signIn & callback

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,9 @@
 export const APP_NAME= "Shop next"
 export const APP_DESCRIPTION= "Ecommerce-test next-js"
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000"
+
+export const signInDefaultValues = {
+    email: "",
+    password: ""
+}
